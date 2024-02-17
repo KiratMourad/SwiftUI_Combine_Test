8376128c84f1f96ae70b118d5ad8919f226cf329
Kirat <mourad.kirat.9@gmail.com>
2024-02-17 14:30:12 +0100

    Initial Commit

---
+++ CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by mac on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
